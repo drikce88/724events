8744372345ba2c4f8a19c93165cc4079e682f2a4
drikce88 <drikce88@gmail.com>
2024-03-21 16:06:08 +0100

    Ajout d'id pour les liens de la nav, correction dee setCollapsed pour que le volet déroulant se ferme bien aprés avoir selectinner la catégorie "toutes".

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -13,12 +13,13 @@ const Select = ({
   label,
   type = "normal",
 }) => {
-  const [value, setValue] = useState();
+  const [value, setValue] = useState(null);
   const [collapsed, setCollapsed] = useState(true);
   const changeValue = (newValue) => {
+    // Changement de onChange en ajoutant newValue
     onChange(newValue);
     setValue(newValue);
-    setCollapsed(newValue);
+    setCollapsed(true);
   };
   return (
     <div className={`SelectContainer ${type}`} data-testid="select-testid">
